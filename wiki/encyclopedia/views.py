9c8ae0baebf8ebb75bdd24d87329a748d700cbb2
Frederico Baker <70851418+FredericoBaker@users.noreply.github.com>
2023-07-31 12:24:14 -0300

    Entry page added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    markdownEntry = util.get_entry(entry)
+    if markdownEntry:
+        html = markdown2.markdown(markdownEntry)
+    else:
+        html = markdownEntry
+    return render(request, "encyclopedia/entry.html", {
+        "title": entry,
+        "content": html
+    })
